f5940e1b34e4321ca6415b10ade9d6d91583a274
Albert843 <nuralbert@yandex.ru>
2023-02-15 05:57:35 +0300

    Added code in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача:
+// Написать программу, которая из имеющегося массива строк формирует массив строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+
+string[] array1 = new string[]{"hello", "2", "world", ":-)"};
+PrintArray(array1);
+string[] array2 = new string[array1.Length];
+GetArray(array1, array2);
+Console.Write("-> ");
+PrintArray(array2);
+//----------Methods----------
+void PrintArray(string[] array){
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+void GetArray(string[] array1, string[] array2){
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3){
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
